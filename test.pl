aed947a3a1dafda67e6f7e0cfa7fdc79eb11bd4d
digitalvan <digitalvan@gmail.com>
2017-11-21 20:11:37 +0000

    Add paths to parent directories of all modules contained in sc-perl.

--- test.pl
+++ test.pl
@@ -1,4 +1,4 @@
-#!/root/test/localperl/bin/perl -w 
+#!/root/test/localperl/bin/perl -w -I /root/sc-perl/localperl/lib/site_perl/5.26.1/Module/Build -I /localperl/lib/5.26.1 -I /root/sc-perl/localperl/lib/5.26.1/x86_64-linux -I /root/sc-perl/localperl/lib/5.26.1 -I /root/sc-perl/localperl/lib/site_perl/5.26.1/x86_64-linux -I /root/sc-perl/localperl/lib/site_perl/5.26.1
 
 use Data::Dumper;
 use DBI;

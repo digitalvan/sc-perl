c756a5e9533be5eda832f9b0191353a6006a580a
digitalvan <digitalvan@gmail.com>
2017-11-22 03:49:13 +0000

    Maintain consistency with perl binary @INC directory.

--- test.pl
+++ test.pl
@@ -1,4 +1,4 @@
-#!./localperl/bin/perl -w -I ./localperl/lib/site_perl/5.26.1/Module/Build -I ./localperl/lib/5.26.1 -I ./localperl/lib/5.26.1/x86_64-linux -I ./localperl/lib/5.26.1 -I ./localperl/lib/site_perl/5.26.1/x86_64-linux -I ./localperl/lib/site_perl/5.26.1
+#!/root/localperl/bin/perl -w -I /root/localperl/lib/site_perl/5.26.1/Module/Build -I /root/localperl/lib/5.26.1 -I /root/localperl/lib/5.26.1/x86_64-linux -I /root/localperl/lib/5.26.1 -I /root/localperl/lib/site_perl/5.26.1/x86_64-linux -I /root/localperl/lib/site_perl/5.26.1
 
 use Data::Dumper;
 use DBI;

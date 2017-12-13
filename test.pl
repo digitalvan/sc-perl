e8157709ff50dba3bc4c3f33fc9dc393ef45eb02
digitalvan <digitalvan@gmail.com>
2017-12-13 08:15:53 +0000

    Add libmysqlclient20 dependencies.

--- test.pl
+++ test.pl
@@ -1,5 +1,20 @@
 #!localperl/bin/perl -w -I localperl/lib/site_perl/5.26.1/Module/Build -I localperl/lib/5.26.1 -I localperl/lib/5.26.1/x86_64-linux -I localperl/lib/5.26.1 -I localperl/lib/site_perl/5.26.1/x86_64-linux -I localperl/lib/site_perl/5.26.1
 
+BEGIN {
+    my $need= 'localperl/lib/x86_64_linux-gnu';
+    my $ld= $ENV{LD_LIBRARY_PATH};
+    if(  ! $ld  ) {
+        $ENV{LD_LIBRARY_PATH}= $need;
+    } elsif(  $ld !~ m#(^|:)\Q$need\E(:|$)#  ) {
+        $ENV{LD_LIBRARY_PATH} .= ':' . $need;
+    } else {
+        $need= "";
+    }
+    if(  $need  ) {
+        exec 'env', $^X, $0, @ARGV;
+    }
+}
+
 use Data::Dumper;
 use DBI;
 use DBD::mysql;
